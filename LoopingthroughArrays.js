5f9c507d16e08c2094bbc2bc501b76f3f48378c4
imanardiansyahh <imanardiansyah89@gmail.com>
2020-11-04 14:42:16 +0800

    hari ini

---
+++ LoopingthroughArrays.js
@@ -0,0 +1,4 @@
+const vacationSpots = ['Parangloe','Pattalassang', 'Bontobiraeng']
+
+for (let i = 0; i < vacationSpots.length; i++)
+console.log('I Would love to visit ' + vacationSpots[i])
---
+++ nestedLooping.js
@@ -0,0 +1,7 @@
+for (let i = 0; i < 10; i++) {
+  for (let k = 0; k < 10; k++) {
+    for (let l = 0; l < 10; l++) {
+      console.log(i, k, l);
+    }
+  }
+}
